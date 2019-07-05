1ae9397e6f2e0540427fc9f7c6f4fb1c79ee60eb
zyh <jt_zyh10000@163.com>
2019-07-05 18:13:52 +0800

    java

---
+++ Java/chapter11/Date/DateDemo1.java
@@ -0,0 +1,9 @@
+import util.DateTime;
+
+public class DateDemo1{
+  public static void main(String args[]){
+    DateTime dt = new DateTime();
+    System.out.println(dt.getDate());
+    System.out.println(dt.getDateComplete());
+  }
+}
